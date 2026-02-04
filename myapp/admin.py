3bea4ab961fb2917a33aa5332f9d3c4107e96123
Prateek Jain <prateekjain276@gmail.com>
2026-02-04 17:10:43 +0530

    Add Models and Databases and Django Admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Student
 
-admin.site.register(Student)
+admin.site.site_header = "Student Management System"
+admin.site.site_title = "Admin Panel"
+admin.site.index_title = "Welcome Admin"
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'age', 'created_at')
+    search_fields = ('name', 'email')
+    list_filter = ('age',)
+
--- myapp/models.py
+++ myapp/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 class Student(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
+    email = models.EmailField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
